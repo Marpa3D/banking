aa2d0d876ca5ab4bfac4a6b3b28dea02731808f8
Marpa <Marpa3d@yandex.ru>
2024-07-07 13:25:35 +0300

    JSON encoding из структуры

--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Customer struct {
+	Name    string `json:"full_name"`
+	City    string `json:"city"`
+	Zipcode string `json:"zip_code"`
+}
+
+func getCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{"Viacheslav",
+			"Zhukovsky",
+			"18185",
+		},
+		{"Mark",
+			"Moskow",
+			"147800",
+		},
+	}
+	w.Header().Add("Content-type", "Aplicattion/json")
+	json.NewEncoder(w).Encode(customers)
+}
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Greeting page</h1>")
@@ -13,6 +35,7 @@ func greet(w http.ResponseWriter, r *http.Request) {
 func main() {
 
 	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getCustomers)
 
 	fmt.Println("Start server...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
